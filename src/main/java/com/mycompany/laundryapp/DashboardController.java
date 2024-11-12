4d1e8b697dfc2b3da5b4ee3164128f00659f8012
Hồ Đình Trí <162857265+trib2203591@users.noreply.github.com>
2024-11-12 10:48:12 +0700

    update

--- src/main/java/com/mycompany/laundryapp/DashboardController.java
+++ src/main/java/com/mycompany/laundryapp/DashboardController.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this templat
  */
 package com.mycompany.laundryapp;
 
